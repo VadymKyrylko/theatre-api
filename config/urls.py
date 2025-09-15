40197d73839a225cc960c79dc4a87efddcb10e66
Vadym <kirilkovadim193@gmail.com>
2025-09-15 17:53:57 +0300

    Refactored urls

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 
 def health_check(request):
     return JsonResponse({"status": "OK"})
@@ -25,4 +28,13 @@ def health_check(request):
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/health/", health_check),
+    path("api/v1/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/v1/user/", include("user.urls", namespace="user")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/v1/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
---
+++ theatre/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import GenreViewSet, ActorViewSet, TheatreHallViewSet, PlayViewSet, PerformanceViewSet, ReservationViewSet
+
+app_name = "theatre"
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
+router.register("plays", PlayViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("reservations", ReservationViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
+
+
